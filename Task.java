4056dcae5a7c3a05c88a83118fb774d05bc584af
Daniyar Alkulov <medvedicabolsaa@gmail.com>
2024-04-14 17:05:56 +0200

    Omitted creating files and launching files - only .csproj and Task.java

---
+++ Task.java
@@ -0,0 +1,8 @@
+class Task {
+    public static void main(String[] args) {
+        System.err.println("itransition");
+    }
+    public static void Main(String[] args) {
+        main(args);
+    }
+}
